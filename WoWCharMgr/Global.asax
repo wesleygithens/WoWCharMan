ed7124c5a7d29fa8c468410afc9ca051bbec4667
wesle <wesle@WESLEYLAPTOP>
2016-11-01 12:51:38 -0400

    Add project files.

---
+++ WoWCharMgr/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WoWCharMgr.MvcApplication" Language="C#" %>
---
+++ WoWCharMgr/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WoWCharMgr.Startup))]
+namespace WoWCharMgr
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
